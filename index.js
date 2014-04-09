5464e0a8c7c453048f40d6d00afe8d84d4008027
elliottcable <me@ell.io>
2014-04-09 09:01:04 -0500

    + switching to promise-style

--- index.js
+++ index.js
@@ -1,5 +1,9 @@
-var connect = require('connect')
-  , request = require('request')
+var connect      = require('connect')
+  , Promise      = require('bluebird')
+  , requestAsync = require('request-promise')
+
+Promise.longStackTraces() // “... a substantial performance penalty.” Okay.
+
 
 var app = connect()
    .use( connect.favicon() )
@@ -16,23 +20,24 @@ var app = connect()
       
       var title = incoming.url.slice(1)
       
-      request.get({
+      requestAsync({
          uri: "http://en.wikipedia.org/w/api.php", json: true, encoding: 'utf8'
        , qs: {format: 'json', action: 'query', prop: 'info', titles: title}
-      }, function(err, _, body){
+      }).done(function(body){
          var pages   = body.query.pages
            , keys    = Object.keys(pages)
            , exists  = (pages[keys[0]].missing == null)
+         
          if (exists) {
             outgoing.statusCode = 301
             outgoing.setHeader('Location', "http://en.wikipedia.org/wiki/" + title)
-            
             return outgoing.end('Bye!') }
+         
          else {
             outgoing.statusCode = 404
             return outgoing.end('Not found on Wikipedia.') }
-      }) // http.client.get en.wikipedia.org
-   
+         
+      }) // requestAsync
    }) // .use function(incoming, outgoing)
 
 
